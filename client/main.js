fd6881b7bfb944c4874fa27d519ea6398abe29b5
tomikorzu <tomykorzu@icloud.com>
2024-08-26 00:04:56 -0300

    feat(.gitignore / .env / client): add the first chat functionalitie

---
+++ client/main.js
@@ -0,0 +1,27 @@
+import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
+const socket = io();
+
+const chat = document.getElementById("chat");
+const form = document.getElementById("form");
+const message = document.getElementById("message");
+const submitBtn = document.getElementById("submit-btn");
+const messagesSended = document.getElementById('messages-sended')
+
+socket.on('chat message', (msg) =>{
+    const item = `<li class="message-received">${msg}</li>`
+    messagesSended.insertAdjacentHTML('beforeend', item)
+    scrollTo()
+})
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (message.value) {
+    socket.emit("chat message", message.value);
+    message.value = "";
+  }
+});
+
+function scrollTo (){
+    messagesSended.scrollTop = messagesSended.scrollHeight
+
+}
--- server/index.js
+++ server/index.js
@@ -1,13 +1,39 @@
 import express from "express";
+import logger from "morgan";
+import { Server } from "socket.io"
+import { createServer } from 'node:http'
 
-const port = process.env.PORT ?? 3000
+const port = process.env.PORT ?? 3000;
 
-const app = express()
-
-app.get('/', (req, res) => {
-    res.send('<h1>Esto es el chat</h1>')
+const app = express();
+const server = createServer(app)
+const io = new Server(server, {
+  connectionStateRecovery: {
+    maxDisconnectionDuration: 10000
+  }
 })
 
-app.listen(port, ()=> {
-    console.log(`Server running on port ${port}`)
+io.on('connection', (socket) =>{
+    console.log('A user has connected')
+
+    socket.on('disconnect', () =>{
+        console.log('A user has disconnected')
+    })
+
+    socket.on('chat message', (msg) =>{
+      io.emit('chat message', msg)
+    })
 })
+
+app.use(express.static(process.cwd() + '/client'));
+
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html');
+});
+
+app.use(logger("dev"));
+
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
