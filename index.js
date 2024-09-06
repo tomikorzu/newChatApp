8baee516162f460da16f3be14e66a708ad77ddda
tomikorzu <tomykorzu@icloud.com>
2024-09-06 19:51:58 -0300

    save changes

---
+++ index.js
@@ -0,0 +1,153 @@
+import express from "express";
+import { server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
+import { get } from "http";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 3000;
+const ADMIN = "Admin";
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const expressServer = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+const usersState = {
+  users: [],
+  setUsers: function (newUsersArray) {
+    this.users = newUsersArray;
+  },
+};
+
+// const io = new server(expressServer, {
+//     cors: {
+//         origin: process.env.NODE_ENV === 'production' ? false: ['https://chat-app-socket-io.herokuapp.com' : 'http://localhost:3000'],
+//     }
+// })
+
+io.on("connection", (socket) => {
+  console.log(`User ${socket.id} connected`);
+
+  socket.emit("message", buildMessage(ADMIN, "Welcome to the chat app!"));
+
+  socket.on("enterRoom", ({ name, room }) => {
+    const prevRoom = getUser(socket.id)?.room;
+
+    if (prevRoom) {
+      socket.leave(prevRoom);
+      io.to(prevRoom).emit(
+        "message",
+        buildMessage(ADMIN, `${name} has left the room`)
+      );
+    }
+    const user = activateUser(socket.id, name, room);
+
+    if (prevRoom) {
+      io.to(prevRoom).emit("userList", {
+        users: getUsersInRoom(prevRoom),
+      });
+    }
+
+    socket.join(user.room);
+
+    socket.emit(
+      "message",
+      buildMessage(ADMIN, `You have joined to the ${room} chat room `)
+    );
+
+    socket.broadcast
+      .to(user.room)
+      .emit("message", buildMessage(ADMIN, `${user.name} has joined the room`));
+
+    io.to(user.room).emit("userList", {
+      users: getUsersInRoom(user.room),
+    });
+
+    io.emit("roomList", {
+      rooms: getAllActiveRooms(),
+    });
+  });
+  socket.on("disconnect", () => {
+    const user = getUser(socket.id);
+    userLeavesApp(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        buildMessage(ADMIN, `${user.name} has left the room`)
+      );
+      io.to(user.room).emit("userList", {
+        users: getUsersInRoom(user.room),
+      });
+      io.emit("roomList", {
+        rooms: getAllActiveRooms(),
+      });
+    }
+    console.log(`User ${socket.id} disconnected`);
+  });
+
+  socket.on("message", ({ name, text }) => {
+    const room = getUser(socket.id)?.room;
+    if (room) {
+      io.to(room).emit("message", buildMessage(name, text));
+    }
+  });
+
+  socket.on("activity", (name) => {
+    const room = getUser(socket.id)?.room; 
+    if (room) {
+        socket.broadcast.to(room).emit("activity", name); 
+    }
+});
+
+  socket.on("enterRoom", (data) => {
+    socket.join(data.room);
+    socket.to(data.room).emit("message", {
+      name: ADMIN,
+      text: `${data.name} has joined the room`,
+    });
+  });
+});
+
+function buildMessage(name, text) {
+  return {
+    name,
+    text,
+    time: new Intl.DateTimeFormat("default", {
+      hour: "numeric",
+      minute: "numeric",
+    }).format(new Date()),
+  };
+}
+
+// user functions
+
+function activateUser(id, name, room) {
+  const user = { id, name, room };
+  usersState.setUsers([
+    ...usersState.users.filter((user) => user.id !== id),
+    user,
+  ]);
+  return user;
+}
+
+function userLeavesApp(id) {
+  usersState.setUsers(usersState.users.filter((user) => user.id !== id));
+}
+
+function getUser(id) {
+  return usersState.users.find((user) => user.id === id);
+}
+
+function getUsersInRoom(room) {
+  return usersState.users.filter((user) => user.room === room);
+}
+
+function getAllActiveRooms() {
+  return Array.from(new Set(usersState.users.map((user) => user.room)));
+}
--- public/app.js
+++ public/app.js
@@ -8,15 +8,34 @@ const roomList = document.querySelector(".room-list");
 const activity = document.querySelector(".activity");
 const chatDisplay = document.querySelector(".chat-display");
 
-function sendMessage (e) {
-    e.preventDefault();
-    if (inputName.value  && inputMsg.value && inputRoom.value ) {
-        socket.emit("message", {
-            'name': inputName.value,
-             'text': inputMsg.value
+function sendMessage(e) {
+  e.preventDefault();
+  if (inputName.value && inputMsg.value && inputRoom.value) {
+    socket.emit("message", {
+      name: inputName.value,
+      text: inputMsg.value,
     });
-        inputMsg.value = "";
-    }
-    inputMsg.focus();
+    inputMsg.value = "";
+  }
+  inputMsg.focus(); 
 }
+function enterRoom(e) {
+  e.preventDefault();
+  if (inputName.value && inputRoom.value) {
+    socket.emit("enterRoom", {
+      name: inputName.value,
+      room: inputRoom.value,
+    });
+  }
+}
+document.querySelector(".form-msg").addEventListener("submit", sendMessage);
+document.querySelector(".form-join").addEventListener("submit", enterRoom);
 
+inputMsg.addEventListener("keypress", function () {
+  socket.emit("activity", inputName.value);
+});
+
+//Listen for messages 
+socket.on('activity', (name) => {
+    activity.textContent 
+})
