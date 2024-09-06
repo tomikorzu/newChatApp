a76f27d81bdd9fe27c10893aeef679f37dbc6dbb
tomikorzu <tomykorzu@icloud.com>
2024-09-06 20:18:39 -0300

    save changes

--- public/app.js
+++ public/app.js
@@ -17,7 +17,7 @@ function sendMessage(e) {
     });
     inputMsg.value = "";
   }
-  inputMsg.focus(); 
+  inputMsg.focus();
 }
 function enterRoom(e) {
   e.preventDefault();
@@ -35,7 +35,58 @@ inputMsg.addEventListener("keypress", function () {
   socket.emit("activity", inputName.value);
 });
 
-//Listen for messages 
+//Listen for messages
+socket.on("activity", (data) => {
+  activity.textContent = "";
+  const { name, text, time } = data;
+  const li = document.createElement("li");
+  li.className = "post";
+  if (name === inputName.value) li.className = "post post--left";
+  if (name !== inputName.value && name !== "Admin")
+    li.className = "post post--right";
+  if (name !== "Admin") {
+    li.innerHTML = `
+        <div class="post__header ${
+          name === nameInput.value
+            ? "post__header--user "
+            : "post__header--reply"
+        }">
+        <span class="post__header--name">${name}</span>
+        <span class="post__header--time">${name}</span>
+        </div>
+        <div class="post__ text">${text}</div> 
+
+      `;
+  } else {
+    li.innerHTML = `
+        <div class="post__text">${text}</div> 
+        
+
+      `;
+  }
+  document.querySelector(".chat-display").appendChild(li);
+
+  chatDisplay.scrollTop = chatDisplay.scrollHeight;
+});
+
+let activityTimer
 socket.on('activity', (name) => {
-    activity.textContent 
+  activity.textContent = `${name} is typing...`;
+  clearTimeout(activityTimer);
+  activityTimer = setTimeout(() => {
+    activity.textContent = '';
+  }, 3000);
 })
+
+function showRooms  (rooms) {
+  roomList.textContent  = '';
+  if (rooms) { 
+    roomList.innerHTML = `<em>Active Rooms:</em>`;
+    rooms.foreach ((room, i) => {
+      roomList.textContent += `${room}`;
+      if (rooms.length > 1 && i !== rooms.length - 1) {
+        roomList.textContent += ',';
+      }
+    });
+  }
+}
