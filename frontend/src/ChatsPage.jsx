bddfe92274c7b03a34393180d7775c31bcd6f85f
tomikorzu <tomykorzu@icloud.com>
2024-09-08 19:28:15 -0300

    a

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -4,7 +4,7 @@ const ChatsPage = (props) => {
   return (
     <div style={{ height: '100vh' }}>
       <PrettyChatWindow
-        projectId="91573235-0b32-49b9-988e-98ba6eab12c3"
+        projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
         username={props.user.username}
         secret={props.user.secret}
         style={{ height: '100%' }}
