e9de080fa6e7157cffc175e765b3fab75cb35be6
tomikorzu <tomykorzu@icloud.com>
2024-09-13 21:10:56 -0300

    fix(vite.config)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "./frontend", // Esto asegura que las rutas sean relativas
   plugins: [react()],
+  server: {
+    open: true, // Abre la app automáticamente en el navegador
+    historyApiFallback: true, // Redirige las rutas a index.html
+  },
 });
